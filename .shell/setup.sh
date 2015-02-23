e699c69e0b604de5b4ae99cdf1d03f6178ed9353
Gilbert Sanchez <gsanchez@fb.com>
2015-02-23 15:39:00 -0800

    Add setup.sh to make this easier in the future

---
+++ .shell/setup.sh
@@ -0,0 +1,14 @@
+# Install oh-my-zsh
+curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
+# Setup VIM
+mkdir -p ~/.vim/swaps
+mkdir -p ~/.vim/autoload ~/.vim/bundle && \
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+# VIM Plugins
+cd ~/.vim/bundle
+git clone git://github.com/Lokaltog/vim-powerline.git
+git clone https://github.com/bling/vim-airline ~/.vim/bundle/vim-airline
+git clone https://github.com/kien/ctrlp.vim.git
+git clone https://github.com/klen/python-mode
+git clone git://github.com/davidhalter/jedi-vim.git
+git clone git://github.com/altercation/vim-colors-solarized.git
