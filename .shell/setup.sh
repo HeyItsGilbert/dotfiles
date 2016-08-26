5488e88e94d7331b228ba7b3555a56b477eb54a5
Gilbert Sanchez <gsanchez@fb.com>
2016-08-26 09:33:33 -0700

    Update the ctrlp.vim source

--- .shell/setup.sh
+++ .shell/setup.sh
@@ -9,6 +9,6 @@ cd ~/.vim/bundle
 # git clone https://github.com/Lokaltog/vim-powerline.git
 git clone https://github.com/bling/vim-airline
 git clone https://github.com/vim-airline/vim-airline-themes
-git clone https://github.com/kien/ctrlp.vim.git
+git clone https://github.com/ctrlpvim/ctrlp.vim
 git clone https://github.com/altercation/vim-colors-solarized.git
 git clone https://github.com/PProvost/vim-ps1.git
