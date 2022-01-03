6e02f2f2b2b1bd5643581b691433eb4bde7c4580
Gilbert Sanchez <gsanchez@fb.com>
2022-01-03 12:55:18 -0800

    Add symlink for windows terminal

--- .shell/setup.ps1
+++ .shell/setup.ps1
@@ -10,6 +10,13 @@ Install-Module PSReadLine -Scope CurrentUser
 Install-Module AdvancedHistory -Scope CurrentUser
 Install-Module Terminal-Icons -Scope CurrentUser
 
+# Create symbolic link for windows terminal settings
+$link = @{
+  ItemType = SymbolicLink
+  Path = "$Env:LocalAppData\Packages\Microsoft.WindowsTerminalPreview_8wekyb3d8bbwe\LocalState\settings.json"
+  Target = .\.shell\WindowsTerminal\settings.json
+}
+New-Item @link
 
 # Install via Store
 # - Account Surfer
