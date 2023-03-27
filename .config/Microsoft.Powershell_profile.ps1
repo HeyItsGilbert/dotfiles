80aace37a140916ef09ab31a52f88bab44d5649a
Gilbert Sanchez <me@gilbertsanchez.com>
2023-03-27 09:45:54 -0700

    Add docker aliases + tmux auto renumber

--- .config/Microsoft.Powershell_profile.ps1
+++ .config/Microsoft.Powershell_profile.ps1
@@ -1,53 +1,17 @@
+if ($env:TERM_PROGRAM -eq "VSCODE") {
+  # Don't load anything else in VSCode. It slows and breaks stuff.
+  Write-Host "VScode!"
+  return
+}
 # Prompt
-Import-Module Posh-Git
 Import-Module Terminal-Icons
 Import-Module PSReadLine
 Import-Module AdvancedHistory
 
-# Run once to load gitprompt setting
-Write-VcsStatus | Out-Null
-$GitPromptSettings.BeforeStatus.Text = ''
-$GitPromptSettings.AfterStatus.Text = ''
-
-# Check if admin
-if (-Not ($IsLinux -or $IsMacOS)) {
-  $currentPrincipal = New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())
-  if ($currentPrincipal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) { $Script:IsAdmin = $true }
-}
-
-# Everything in the prompt will be executed on each new line, be smart!
-function prompt {
-  if (((Get-Item $pwd).parent.parent.name)) {
-    $Path = '..\' + (Get-Item $pwd).parent.name + '\' + (Split-Path $pwd -Leaf)
-  } else {
-    $Path = $pwd.path
-  }
-
-  $lastRun = (Get-History -Count 1).Duration.TotalSeconds
-  if ($lastRun) {
-    Write-Host "[$($lastRun)s] " -ForegroundColor Black -BackgroundColor Green -NoNewline
-  }
-
-  if ($Script:IsAdmin) {
-    Write-Host "$([char]0x26a1)" -ForegroundColor Black -BackgroundColor Green -NoNewline
-    Write-Host "$([char]0xE0B0)$([char]0xE0B1)" -ForegroundColor Green -BackgroundColor DarkBlue -NoNewline
-  } else {
-    Write-Host "$([char]0x1f476)" -ForegroundColor Black -BackgroundColor Green -NoNewline
-    Write-Host "$([char]0xE0B0)$([char]0xE0B1)" -ForegroundColor Green -BackgroundColor DarkBlue -NoNewline
-  }
-
-  Write-Host " $($MyInvocation.HistoryId)" -ForegroundColor White -BackgroundColor DarkBlue -NoNewline
-  Write-Host "$([char]0xE0B0)$([char]0xE0B1) " -ForegroundColor DarkBlue -BackgroundColor Cyan -NoNewline
-  Write-Host ($path).ToLower().TrimEnd('\') -ForegroundColor White -BackgroundColor Cyan -NoNewline
-  if ((Write-VcsStatus *>&1).Length -gt 0) {
-    Write-Host "$([char]0xE0B0)$([char]0xE0B1)" -ForegroundColor Cyan -BackgroundColor Magenta -NoNewline
-    Write-Host "$(Write-VcsStatus)" -BackgroundColor Magenta -NoNewline
-    #& $GitPromptScriptBlock
-    Write-Host "$([char]0xE0B0)$("$([char]0xE0B1)" * $NestedPromptLevel)" -ForegroundColor Magenta -NoNewline
-  } else {
-    Write-Host "$([char]0xE0B0)$("$([char]0xE0B1)" * $NestedPromptLevel)" -ForegroundColor Cyan -NoNewline
-  }
-  ' '
+# Load work functions
+$wf = Resolve-Path -Relative "$PSScriptRoot\WorkFunctions.ps1"
+if (Test-Path $wf) {
+  . $wf
 }
 
 # Aliases
@@ -89,15 +53,6 @@ if (Test-Path($ChocolateyProfile)) {
   Import-Module "$ChocolateyProfile"
 }
 
-function Send-Ping {
-  [alias('pingme')]
-  param ([String]$Message)
-  $msg = @{'m' = $Message } | ConvertTo-Json -Compress
-  $variables = $msg -replace '"', '\"'
-  $query = 'query($m: String!) { pingme(message: $m) }'
-  jf graphql --query $query --variables $variables
-}
-
 function Watch-Command {
   [alias('watch')]
   [CmdletBinding()]
@@ -117,6 +72,55 @@ function Watch-Command {
   }
 }
 
+# Don't bother changing prompts if we're just using starship
 if (Get-Command 'starship') {
   Invoke-Expression (&starship init powershell)
+} else {
+  Import-Module Posh-Git
+
+  # Run once to load gitprompt setting
+  Write-VcsStatus | Out-Null
+  $GitPromptSettings.BeforeStatus.Text = ''
+  $GitPromptSettings.AfterStatus.Text = ''
+
+  # Check if admin
+  if (-Not ($IsLinux -or $IsMacOS)) {
+    $currentPrincipal = New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())
+    if ($currentPrincipal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) { $Script:IsAdmin = $true }
+  }
+
+  # Everything in the prompt will be executed on each new line, be smart!
+  function prompt {
+    if (((Get-Item $pwd).parent.parent.name)) {
+      $Path = '..\' + (Get-Item $pwd).parent.name + '\' + (Split-Path $pwd -Leaf)
+    } else {
+      $Path = $pwd.path
+    }
+
+    $lastRun = (Get-History -Count 1).Duration.TotalSeconds
+    if ($lastRun) {
+      Write-Host "[$($lastRun)s] " -ForegroundColor Black -BackgroundColor Green -NoNewline
+    }
+
+    if ($Script:IsAdmin) {
+      Write-Host "$([char]0x26a1)" -ForegroundColor Black -BackgroundColor Green -NoNewline
+      Write-Host "$([char]0xE0B0)$([char]0xE0B1)" -ForegroundColor Green -BackgroundColor DarkBlue -NoNewline
+    } else {
+      Write-Host "$([char]0x1f476)" -ForegroundColor Black -BackgroundColor Green -NoNewline
+      Write-Host "$([char]0xE0B0)$([char]0xE0B1)" -ForegroundColor Green -BackgroundColor DarkBlue -NoNewline
+    }
+
+    Write-Host " $($MyInvocation.HistoryId)" -ForegroundColor White -BackgroundColor DarkBlue -NoNewline
+    Write-Host "$([char]0xE0B0)$([char]0xE0B1) " -ForegroundColor DarkBlue -BackgroundColor Cyan -NoNewline
+    Write-Host ($path).ToLower().TrimEnd('\') -ForegroundColor White -BackgroundColor Cyan -NoNewline
+    if ((Write-VcsStatus *>&1).Length -gt 0) {
+      Write-Host "$([char]0xE0B0)$([char]0xE0B1)" -ForegroundColor Cyan -BackgroundColor Magenta -NoNewline
+      Write-Host "$(Write-VcsStatus)" -BackgroundColor Magenta -NoNewline
+      #& $GitPromptScriptBlock
+      Write-Host "$([char]0xE0B0)$("$([char]0xE0B1)" * $NestedPromptLevel)" -ForegroundColor Magenta -NoNewline
+    } else {
+      Write-Host "$([char]0xE0B0)$("$([char]0xE0B1)" * $NestedPromptLevel)" -ForegroundColor Cyan -NoNewline
+    }
+    ' '
+  }
 }
