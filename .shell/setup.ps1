b11896ae96a85116ac73bd7b63e9c06ae391f89c
Gilbert Sanchez <me@gilbertsanchez.com>
2023-05-04 13:37:18 -0700

    Update setup.ps1
    Resolve the paths. Test if modules are installed. Don't force choco install.

--- .shell/setup.ps1
+++ .shell/setup.ps1
@@ -1,9 +1,5 @@
 # Choco packages
-# ToDo: Setup community repo if its not setup
-choco install nerdfont-hack -s'https://chocolatey.org/api/v2/'
-choco install starship -fy -s'https://chocolatey.org/api/v2/'
-choco install powershell-core -fy -s'https://chocolatey.org/api/v2/'
-choco install vscode -fy -s'https://chocolatey.org/api/v2/'
+choco install nerdfont-hack starship powershell-core vscode -y -s'https://chocolatey.org/api/v2/'
 
 # Install Modules
 $mods = @{
@@ -11,26 +7,33 @@ $mods = @{
   'Repository' = 'PSGallery'
   'Force' = $True
 }
-Install-Module Pester @mods
-Install-Module Posh-Git @mods
-Install-Module PSReadLine @mods
-Install-Module AdvancedHistory @mods
-Install-Module Terminal-Icons @mods
-
+# ToDo: Allow additional parameters per module
+@(
+  'Pester',
+  'Posh-Git',
+  'PSReadLine',
+  'AdvancedHistory',
+  'Terminal-Icons',
+  'AnyPackage'
+) | ForEach-Object {
+  if (-Not (Get-Module -ListAvailable -Name $_)) {
+    Install-Module $_ @mods
+  }
+}
 $links = @(
   # Create symbolic links for windows terminal settings
   @{
-    'src' = "$PSScriptRoot\WindowsTerminal\settings.json"
-    'dst' = "$Env:LocalAppData\Packages\Microsoft.WindowsTerminalPreview_8wekyb3d8bbwe\LocalState\settings.json"
+    'src' = Resolve-Path "$PSScriptRoot\WindowsTerminal\settings.json"
+    'dst' = Resolve-Path "$Env:LocalAppData\Packages\Microsoft.WindowsTerminalPreview_8wekyb3d8bbwe\LocalState\settings.json"
   },
   # Create symbolic links for windows terminal settings
   @{
-    'src' = "$PSScriptRoot\..\.config\Microsoft.Powershell_profile.ps1"
-    'dst' = $PROFILE
+    'src' = Resolve-Path "$PSScriptRoot\..\.config\Microsoft.Powershell_profile.ps1"
+    'dst' = Resolve-Path $PROFILE
   }
   @{
-    'src' = "$PSScriptRoot\..\.config\starship.toml"
-    'dst' = "$HOME\.config\starship.toml"
+    'src' = Resolve-Path "$PSScriptRoot\..\.config\starship.toml"
+    'dst' = Resolve-Path "$HOME\.config\starship.toml"
   }
 )
 
@@ -38,6 +41,11 @@ New-Item -ItemType Directory "$HOME\.config"
 $links | ForEach-Object {
   # Test path
   if(Test-Path $_.dst) {
+    $current = Get-Item $_.dst
+    if($current.LinkType -eq 'SymbolicLink' -And $current.Target -eq $_.src){
+      Write-Host ("Symlink already setup for {0}" -F $_.dst)
+      continue
+    }
     Write-Host ("File exists, let's rename prior to creating symlink: {0}" -f $_.dst)
     # If it exists, remove it
     $new = Split-Path $_.dst -Leaf
