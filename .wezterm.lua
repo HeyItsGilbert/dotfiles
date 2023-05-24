40a20e5025b89b68757881accd9565f26b9e3539
Gilbert Sanchez <gilbert@fb.com>
2023-05-24 10:29:31 -0700

    Add PowerShell to wezterm launch menu

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,12 +1,38 @@
 local wezterm = require 'wezterm'
 local mux = wezterm.mux
 local act = wezterm.action
-
 local config = {}
+local launch_menu = {}
+local haswork,work = pcall(require,"work")
 
+--- Setup PowerShell options
+--- Windows PowerShell First
+if wezterm.target_triple == 'x86_64-pc-windows-msvc' then
+  table.insert(launch_menu, {
+    label = 'PowerShell',
+    args = { 'powershell.exe', '-NoLogo' },
+  })
+  table.insert(launch_menu, {
+    label = 'Pwsh',
+    args = { 'pwsh.exe', '-NoLogo' },
+  })
+else
+  table.insert(launch_menu, {
+    label = 'Pwsh',
+    args = { '/usr/local/bin/pwsh', '-NoLogo' },
+  })
+end
+
+--- Default config settings
 config.color_scheme = 'Solarized Dark (Gogh)'
 config.font = wezterm.font('Hack Nerd Font')
 config.font_size = 11
 config.line_height = 1.2
+config.launch_menu = launch_menu
+
+-- Allow overwriting for work stuff
+if haswork then
+  work.apply_to_config(config)
+end
 
 return config
