fa76ef762328ae7e94445fcb7028d29e464f306c
Gilbert Sanchez <gsanchez@fb.com>
2023-07-05 14:52:49 -0700

    Smarter setup.sh

--- setup.sh
+++ setup.sh
@@ -1,20 +1,52 @@
 #!/bin/bash
+
 parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 
+# Check for zsh
+if [[ `which zsh &>/dev/null && $?` != 0 ]]
+then
+  echo "ZSH is not installed! Install it and try again."
+fi
+
 # Install oh-my-zsh
-curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
+if [ -d ~/.oh-my-zsh ]; then
+	echo "oh-my-zsh is installed"
+else
+ 	echo "Installing oh-my-zsh"
+  curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
+fi
+
+# Check for Tmux and Setup
+if [[ `which tmux &>/dev/null && $?` != 0 ]]
+then
+  echo "Tmux is not installed! Install it and try again."
+fi
+# Setup TPM
+if [ -d "~/.tmux/plugins/tpm" ]
+then
+  echo "TPM is already installed."
+else
+  echo "Installing TPM"
+  git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+fi
+
 # Setup VIM
-mkdir -p ~/.vim/swaps
-mkdir -p ~/.vim/autoload ~/.vim/bundle && \
+mkdir -p ~/.vim/swaps ~/.vim/autoload ~/.vim/bundle
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+
 # VIM Plugins
-cd ~/.vim/bundle
-# git clone https://github.com/Lokaltog/vim-powerline.git
-git clone https://github.com/bling/vim-airline
-git clone https://github.com/vim-airline/vim-airline-themes
-git clone https://github.com/ctrlpvim/ctrlp.vim
-git clone https://github.com/altercation/vim-colors-solarized.git
-git clone https://github.com/PProvost/vim-ps1.git
+declare -A vimplugins
+vimplugins=(
+  ["bling/vim-airline"]="vim-airline"
+  ["vim-airline/vim-airline-themes"]="vim-airline-themes"
+  ["ctrlpvim/ctrlp.vim"]="ctrlp.vim"
+  ["altercation/vim-colors-solarized.git"]="vim-colors-solarized"
+  ["/PProvost/vim-ps1.git"]="vim-ps1"
+)
+for plugin in "${!vimplugins[@]}";
+do
+  echo "git clone https://github.com/$plugin - ${vimplugins[$sound]}"
+done
 
 # Make the symlinks
 Links=(
@@ -25,7 +57,12 @@ Links=(
   .shell
   .wezterm
 )
-for i in "${arr[@]}"
+for i in "${Links[@]}"
 do
-  ln -s $PWD/$i ~/$i
+  if [ -h $i ]; then
+    echo "$i is configured"
+  else
+    echo "Linking $i"
+    ln -s $PWD/$i ~/$i
+  fi
 done
