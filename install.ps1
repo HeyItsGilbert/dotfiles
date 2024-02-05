5c9b44fa64f326cc598f73b174752c8caf0979eb
Gilbert Sanchez <me@gilbertsanchez.com>
2024-02-05 06:22:31 -0800

    Add installers

---
+++ install.ps1
@@ -0,0 +1,3 @@
+(Invoke-RestMethod -UseBasicParsing https://get.chezmoi.io/ps1) | powershell -c -
+
+chezmoi init --apply HeyItsGilbert
---
+++ install.sh
@@ -0,0 +1,1 @@
+sh -c "$(curl -fsLS get.chezmoi.io)" -- init --apply $GITHUB_USERNAME
