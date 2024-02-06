64bb7ac2e7aeb55409451397a7b3dff4c0a994a9
Gilbert Sanchez <me@gilbertsanchez.com>
2024-02-06 06:53:06 -0800

    Update install scripts to install apps

--- README.md
+++ README.md
@@ -1,10 +1,9 @@
 # dotfiles
 
-Gilbert's dot files. Thanks to @mikedodge04 for helping me setup a bunch of
-these a while ago.
+HeyItsGilbert's dot files!
 
-Please note that there may be files from other repositories. These files are
-have been accumalted over a long period of time. If you find some code the
+Please note that there may be files from other repositories. These files have
+been accumulated over a long period of time. If you find some code the
 belongs to someone else, please let me know so I can properly attribute them.
 
 ## Pre-Requisites
--- install.ps1
+++ install.ps1
@@ -1,3 +1,36 @@
-(Invoke-RestMethod -UseBasicParsing https://get.chezmoi.io/ps1) | powershell -c -
+$ErrorActionPreference = 'Stop'
+# Choco packages
+choco install chezmoi espanso nerdfont-hack starship powertoys powershell-core vale vscode wezterm -y -s'https://chocolatey.org/api/v2/'
+choco install ditto --pre -y -s'https://chocolatey.org/api/v2/'
 
+# Install via Store
+# - Account Surfer
+# - Windows Terminal Preview
+# - Ditto Clipboard
+# - WSL
+
+# Install Modules
+$mods = @{
+  'Scope' = 'CurrentUser'
+  'Repository' = 'PSGallery'
+  #'Force' = $True
+}
+# ToDo: Allow additional parameters per module
+@(
+  'AdvancedHistory',
+  'DynamicTitle',
+  'Microsoft.PowerShell.ConsoleGuiTools',
+  'Pester',
+  'Posh-Git',
+  'PSReadLine',
+  'PSStyle',
+  'Terminal-Icons'
+) | ForEach-Object {
+  if (-Not (Get-Module -ListAvailable -Name $_)) {
+    Install-Module $_ @mods -Confirm:$False -Force
+  }
+}
+
+# Install and apply chezmoi files
+(Invoke-RestMethod -UseBasicParsing https://get.chezmoi.io/ps1) | powershell -c -
 chezmoi init --apply HeyItsGilbert
