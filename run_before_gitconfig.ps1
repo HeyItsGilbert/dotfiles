22d28c3a63b55eaff169d9593a84462c60dd306a
Gilbert Sanchez <me@gilbertsanchez.com>
2024-02-12 19:02:57 -0800

    Add gitconfig and start of script

---
+++ run_before_gitconfig.ps1
@@ -0,0 +1,10 @@
+#!/usr/bin/env pwsh
+# .gitconfig hash: {{ include ".gitconfig" | sha256sum }}
+
+# Get-Content ~\.gitconfig
+# ToDo: Get all repos and add maintenance
+# [maintenance]
+#   repo = D:/foo
+#   repo = D:/bar
+
+# Add safe repos
