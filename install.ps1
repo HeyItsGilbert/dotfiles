0e69dc3fe53b7be62c7b93c89f61d2cb45da4ccd
Gilbert Sanchez <me@gilbertsanchez.com>
2024-02-13 20:31:00 -0800

    Move package install to run script

--- install.ps1
+++ install.ps1
@@ -1,36 +1,4 @@
 $ErrorActionPreference = 'Stop'
-# Choco packages
-
-choco install 1password-cli chezmoi espanso nerdfont-hack starship powertoys powershell-core vale vscode wezterm -y -s'https://chocolatey.org/api/v2/'
-choco install ditto --pre -y -s'https://chocolatey.org/api/v2/'
-
-# Install via Store
-# - Account Surfer
-# - Windows Terminal Preview
-# - Ditto Clipboard
-# - WSL
-
-# Install Modules
-$mods = @{
-  'Scope' = 'CurrentUser'
-  'Repository' = 'PSGallery'
-  #'Force' = $True
-}
-# ToDo: Allow additional parameters per module
-@(
-  'AdvancedHistory',
-  'DynamicTitle',
-  'Microsoft.PowerShell.ConsoleGuiTools',
-  'Pester',
-  'Posh-Git',
-  'PSReadLine',
-  'PSStyle',
-  'Terminal-Icons'
-) | ForEach-Object {
-  if (-Not (Get-Module -ListAvailable -Name $_)) {
-    Install-Module $_ @mods -Confirm:$False -Force
-  }
-}
 
 # Install and apply chezmoi files
 (Invoke-RestMethod -UseBasicParsing https://get.chezmoi.io/ps1) | powershell -c -
---
+++ run_once_before_psmodules.ps1
@@ -0,0 +1,4 @@
+#!/usr/bin/env pwsh
+
+Install-Module PowerShellGet -Force -AllowClobber
+Install-Module Microsoft.PowerShell.PSResourceGet -Repository PSGallery
