1187d90b9e71718bb6c1e98d2d2da232e1fea560
Gilbert Sanchez <gilbsanchez@tesla.com>
2026-05-13 12:12:46 -0700

    fix(powershell): 🐛 Ensure transient prompt is only disabled if available
    * Added a check for the "Disable-TransientPrompt" command before calling it.
    * This prevents potential errors if the command is not present.

--- dot_config/powershell/Microsoft.VSCode_profile.ps1
+++ dot_config/powershell/Microsoft.VSCode_profile.ps1
@@ -2,4 +2,7 @@ Write-Host "Loading Gilbert's VSCode profile"
 Import-CommandSuite
 
 # Transient prompt breaks the prompt detection
-Disable-TransientPrompt
+if (Get-Command "Disable-TransientPrompt" -ErrorAction SilentlyContinue) {
+    Write-Host "Disabling transient prompt"
+    Disable-TransientPrompt
+}
